6ab92ec5dbe51e508a0010b4118114ee3949efd7
Keith Adams <kma@slack-corp.com>
2020-03-24 19:37:59 -0700

    So close to things working.

--- grunk.cu
+++ grunk.cu
@@ -4,27 +4,55 @@
 #include <stdio.h>
 #include <stdint.h>
 
+typedef struct Offset2D {
+  size_t x, y;
+} Offset2ds;
+
+__device__
+size_t computeModularOffset(size_t x, size_t y, size_t numRows, size_t numCols) {
+  return (x % numCols) * numCols + (y % numRows);
+}
+
+__device__
+Offset2D computeOffset(dim3 tileDim, dim3 blockIdx, uint3 threadIdx, size_t numRows, size_t numCols) {
+  auto row = tileDim.y * blockIdx.y + threadIdx.y;
+  auto col = tileDim.x * blockIdx.x + threadIdx.x;
+  return Offset2D { col, row };
+}
+
 // Square stencil.
 template<typename Payload, typename Op, int kBlockHeight, int kBlockWidth>
 __global__
-void apply2dStencil(const Payload* in, Payload* out, int numRows, int numCols) {
-  // XXX: build a shared "skirt" around the block.
+void apply2dStencil(const Payload* in, Payload* out, dim3 tileDim, int numRows, int numCols) {
+  auto off2 = computeOffset(tileDim, blockIdx, threadIdx, numRows, numCols);
+  if (true || blockIdx.x == 3 && blockIdx.y == 1){
+    // printf("gridDim: %d,%d\n", gridDim.x, gridDim.y);
+    printf("b(%d, %d) t(%d, %d)-> %d,%d\n", blockIdx.x, blockIdx.y,
+           threadIdx.x, threadIdx.y, int(off2.x), int(off2.y));
+  }
 
-  // const int blockUpperLeft = kBlockHeight * gridDim.x * blockIdx.y + blockIdx.x * kBlockWidth;
-  // printf("(%d, %d) -> %d\n", blockIdx.x, blockIdx.y, blockUpperLeft);
-  const int center = threadIdx.y * blockDim.x * gridDim.x + threadIdx.x;
-  // printf("b(%d, %d) t(%d, %d)-> %d\n", blockIdx.x, blockIdx.y,
-  //        threadIdx.x, threadIdx.y, center);
-
-  // XXX: it's lame that we're screwing up the border.
-  const int north = threadIdx.y >= 1 ? (threadIdx.y - 1) * blockDim.x +
-    threadIdx.x : center;
-  const int south = threadIdx.y < numCols - 1 ? (threadIdx.y + 1) * blockDim.x +
-    threadIdx.x : center;
   Op op;
-  out[center] = op.op(in[north - 1],  in[north], out[north + 1],
-                      in[center - 1], in[center], out[center + 1],
-                      in[south - 1],  in[south], out[south + 1]);
+  if (off2.x >= numCols) return;
+  if (off2.y >= numRows) return;
+
+  auto center = computeModularOffset(off2.x, off2.y, numRows, numCols);
+  if (false && off2.x >= 1 && off2.x < numCols - 1 &&
+      off2.y >= 1 && off2.y < numRows - 1) {
+    auto north = center - numCols;
+    auto south = center + numCols;
+    out[center] = op.op(in[north - 1],  in[north], out[north + 1],
+                        in[center - 1], in[center], out[center + 1],
+                        in[south - 1],  in[south], out[south + 1]);
+    return;
+  }
+
+  auto nudge = [&](int dx, int dy) {
+    return computeModularOffset(off2.x + dx, off2.y + dy, numRows, numCols);
+  };
+  out[center] = op.op(
+        in[nudge(-1, -1)], in[nudge(0, -1)], in[nudge(+1, -1)],  // nw, north, ne
+        in[nudge(-1,  0)], in[center],       in[nudge( 0, +1)],  // w        , e
+        in[nudge(-1, +1)], in[nudge(0, +1)], in[nudge(+1, +1)]); // sw, south, se
 }
 
 typedef uint8_t Cell;
@@ -50,8 +78,9 @@ class GPULife {
     : m_numRows(numRows)
     , m_numCols(numCols)
   {
-    cudaMalloc(&m_gpuCells, 2 * numRows * numCols * sizeof(Cell));
-    m_gpuCellsOut = m_gpuCells + numRows * numCols * sizeof(Cell);
+    const auto boardSize = numRows * numCols * sizeof(Cell);
+    cudaMalloc(&m_gpuCells, 2 * boardSize);
+    m_gpuCellsOut = m_gpuCells + boardSize;
 
     m_hCells = (Cell*)malloc(sizeof(Cell) * numRows * numCols);
 
@@ -75,19 +104,38 @@ class GPULife {
   void gen() {
     static const int kBlockWidth = 16;
     static const int kBlockHeight = 16;
-    const dim3 grid(ceilDiv(m_numCols, kBlockWidth),
-                       ceilDiv(m_numRows, kBlockHeight));
+    const dim3 grid(ceilDiv(m_numCols, kBlockHeight),
+                    ceilDiv(m_numRows, kBlockWidth));
     const dim3 blk(kBlockWidth, kBlockHeight);
-    apply2dStencil<Cell, GameOfLifeOp, kBlockHeight, kBlockHeight><<<grid, blk>>>(m_gpuCells, m_gpuCellsOut, m_numRows, m_numCols);
+    const dim3 tile(kBlockWidth, kBlockHeight);
+    apply2dStencil<Cell, GameOfLifeOp, kBlockWidth, kBlockHeight><<<grid, blk>>>(m_gpuCells, m_gpuCellsOut, tile, m_numRows, m_numCols);
+    // Flip the buffer
     Cell* temp = m_gpuCells;
     m_gpuCells = m_gpuCellsOut;
     m_gpuCellsOut = temp;
-    cudaMemcpy(m_hCells, m_gpuCells, sizeof(Cell) * m_numRows * m_numCols,
-               cudaMemcpyDeviceToHost);
   }
 
   void show() const {
+    cudaMemcpy(m_hCells, m_gpuCells, sizeof(Cell) * m_numRows * m_numCols,
+               cudaMemcpyDeviceToHost);
+    // Headers across top; first, column 10s
+    printf("%14s", " ");
+    for (int i = 10; i < m_numCols; i++){
+      int head = i / 10;
+      if (i % 10 == 0) printf("%d", head); else printf(" ");
+    }
+    printf("\n");
+
+    // column 1's
+    printf("    ");
+    for (int i = 0; i < m_numCols; i++){
+      int head = i % 10;
+      printf("%d", head);
+    }
+    printf("\n");
+
     for (int i = 0; i < m_numRows; i++) {
+      printf("%-4d", i);
       for (int j = 0; j < m_numCols; j++) {
         bool val = bool(m_hCells[i * m_numCols + j]);
         printf("%c", val ? '#' : ' ');
