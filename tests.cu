71e9a319f13ec116202c3edd289ccbd323a1343f
Keith Adams <kma@slack-corp.com>
2020-03-26 15:40:38 -0700

    Apron busted.

--- tests.cu
+++ tests.cu
@@ -1,25 +1,45 @@
 #include <stdio.h>
-#include <assert.h>
+#include <string>
+#include <vector>
 
 #include "gpulife.hpp"
+
+std::vector<std::string> errors;
+
+#define asrt(x) do {   \
+  auto pred = bool(x); \
+  if (!pred) {         \
+    errors.push_back(std::string(#x)); \
+  }                    \
+} while(0)
 
 void test_modular_offset(){
   // A 2x3 grid:
   //    _____
   //   |0|1|2|
   //   |3|4|5|
-  assert(MODULAR_OFFSET(0, 0, 2, 3) == 0);
-  assert(MODULAR_OFFSET(0, 1, 2, 3) == 1);
-  assert(MODULAR_OFFSET(0, 2, 2, 3) == 2);
-  assert(MODULAR_OFFSET(1, 0, 2, 3) == 3);
-  assert(MODULAR_OFFSET(1, 1, 2, 3) == 4);
-  assert(MODULAR_OFFSET(1, 2, 2, 3) == 5);
+  asrt(MODULAR_OFFSET(0, 0, 2, 3) == 0);
+  asrt(MODULAR_OFFSET(0, 1, 2, 3) == 1);
+  asrt(MODULAR_OFFSET(0, 2, 2, 3) == 2);
+  asrt(MODULAR_OFFSET(1, 0, 2, 3) == 3);
+  asrt(MODULAR_OFFSET(1, 1, 2, 3) == 4);
+  asrt(MODULAR_OFFSET(1, 2, 2, 3) == 5);
+
+  // The "apron": negative numbers
+  asrt(MODULAR_OFFSET(-1, -1, 2, 3) == 5);
+
 }
 
 int main(int argc, char** argv){
-  assert(1);
+  asrt(1);
   test_modular_offset();
-  printf("w00t\n");
-  return 0;
+  if (errors.size() == 0){
+    printf("w00t\n");
+    return 0;
+  }
+
+  for (auto e: errors) {
+    fprintf(stderr, "Error: %s\n", e.c_str());
+  }
 }
 
